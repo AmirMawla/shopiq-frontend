c296899a7f712282ae73cb54e8f9b41bba2a07df
donia <doniaamohamed498@gmail.com>
2026-03-31 13:13:45 +0200

    Fixed product-details template and Add to Cart logic

--- src/app/components/features/customer/product-details/product-details.ts
+++ src/app/components/features/customer/product-details/product-details.ts
@@ -7,6 +7,7 @@ import { Category } from '../../../../models/category';
 import { ProductService } from '../../../../services/product';
 import { CategoriesS} from '../../../../services/category';
 import { Router } from '@angular/router';
+import { CartService } from '../../../../services/cart';
 @Component({
   selector: 'app-product-details',
   imports: [RouterLink, AsyncPipe, JsonPipe],
@@ -18,14 +19,41 @@ export class ProductDetails implements OnInit {
   private apiProductsSErvice = inject(ProductService)
   private cdr = inject(ChangeDetectorRef)
   private router = inject(Router);
-
+  private cartService = inject(CartService);
+  isInCart = false;
   product$!: Observable<Product>
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['id'];
     this.product$ = this.apiProductsSErvice.getProductById(id)
+    
+    this.cartService.isItemInCart(id).subscribe({
+    next: (res) => {
+      this.isInCart = res?.inCart || false;
+      this.cdr.detectChanges();
+    },
+    error: () => {
+      this.isInCart = false;
+      this.cdr.detectChanges();
+    }
+  });
 
   }
+  addToCart(productId: string) {
+  this.cartService.addItem(productId, 1).subscribe({
+    next: () => {
+      this.isInCart = true;
+      alert('Product added to cart 🛒');
+      this.cdr.detectChanges();
+    },
+    error: (err) => {
+      console.error(err);
+      this.cdr.detectChanges();
+    }
+  });
+  }
+
   goBack() {
-    this.router.navigate(['/customer/products']);
+    this.router.navigate(['/products']);
+    this.cdr.detectChanges();
   }
 }
