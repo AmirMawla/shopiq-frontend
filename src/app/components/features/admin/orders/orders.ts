028839b724f9744e1205206b576093a9f57a4b97
hagernofal <hagernofal8@gmail.com>
2026-03-31 13:57:49 +0200

    feat: add admin orders page

--- src/app/components/features/admin/orders/orders.ts
+++ src/app/components/features/admin/orders/orders.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AdminService } from '../../../../services/admin';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  standalone: true,
+  imports: [DatePipe],
   templateUrl: './orders.html',
-  styleUrl: './orders.css',
+  styleUrl: './orders.css'
 })
-export class Orders {}
+export class Orders {
+  private adminService = inject(AdminService);
+
+  orders: any[] = [];
+  loading = true;
+  error = '';
+
+  constructor() {
+    this.loadOrders();
+  }
+
+  loadOrders() {
+    this.loading = true;
+    this.adminService.getAllOrders().subscribe({
+      next: (res: any) => {
+        console.log('Orders response:', res);
+        this.orders = res.data.items;
+        this.loading = false;
+      },
+      error: (err: any) => {
+        console.error('Error loading orders:', err);
+        this.error = 'Failed to load orders';
+        this.loading = false;
+      }
+    });
+  }
+}
